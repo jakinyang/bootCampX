90fd0cdfa1ddf3e43d96ac8cf7e8c25c89d52347
jakinyang <109392405+jakinyang@users.noreply.github.com>
2022-11-16 19:37:42 -0800

    Complete queries for students.js

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,21 +7,33 @@ const pool = new Pool({
   database: 'bootcampx',
 });
 
+const cohortId = process.argv[2];
+console.log(cohortId);
+const queryLimit = process.argv[3];
+console.log(queryLimit);
+
 pool.connect()
   .then(()=> {
-    console.log(`Pool connection succesful: `, pool);
+    console.log(`Pool connection succesful`);
   })
   .catch((error) => {
     console.log(`Pool connection error: `, error);
   })
 
 pool.query(
-    `SELECT id, name, cohort_id
+    `SELECT students.id, students.name AS student, cohorts.name AS cohort
     FROM students
-    LIMIT 5;`
+    JOIN cohorts
+    ON cohorts.id = students.cohort_id
+    WHERE cohorts.name LIKE '%${cohortId}%'
+    LIMIT ${queryLimit};`
 )
   .then((res) => {
-    console.log(`Pool query responnse: `, res);
+    console.table(/* `Pool query response rows: `,  */res.rows);
+    res.rows.forEach(row => {
+      console.log(`${row.student} has an id of ${row.id} and is in the ${row.cohort} cohort`)
+    })
+    pool.end();
   })
   .catch((err) => {
     console.log(`Pool query error: `, err);
